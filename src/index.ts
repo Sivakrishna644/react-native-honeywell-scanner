445c29516f2fdab9f677db9b87a2f047aaa7883a
Siva Krishna Singasani <sivakrishna.singasani@atmecs.com>
2021-07-09 22:04:58 +0530

    restored original file as modifications were not working

--- src/index.ts
+++ src/index.ts
@@ -13,27 +13,32 @@ const barcodeReaderEmitter = new NativeEventEmitter(HoneywellScanner);
 var subscriptionBarcodeReadSuccess = null;
 var subscriptionBarcodeReadFail = null;
 
-HoneywellScanner.onBarcodeReadSuccess = (handler) => {
-    subscriptionBarcodeReadSuccess === null || subscriptionBarcodeReadSuccess === void 0 ? void 0 : subscriptionBarcodeReadSuccess.remove();
+HoneywellScanner.onBarcodeReadSuccess = (handler) =>
+{
+    subscriptionBarcodeReadSuccess.remove();
     subscriptionBarcodeReadSuccess = null;
     subscriptionBarcodeReadSuccess = barcodeReaderEmitter.addListener(HoneywellScanner.BARCODE_READ_SUCCESS, handler);
-};
-HoneywellScanner.onBarcodeReadFail = (handler) => {
-    subscriptionBarcodeReadFail === null || subscriptionBarcodeReadFail === void 0 ? void 0 : subscriptionBarcodeReadFail.remove();
+}
+
+HoneywellScanner.onBarcodeReadFail = (handler) =>
+{
+    subscriptionBarcodeReadFail.remove();
     subscriptionBarcodeReadFail = null;
     subscriptionBarcodeReadFail = barcodeReaderEmitter.addListener(HoneywellScanner.BARCODE_READ_FAIL, handler);
-};
+}
 
 /**
  * Stop listening for event
  * @param  {String} eventName Name of event one of barcodeReadSuccess, barcodeReadFail
  * @param  {Function} handler Event handler
  */
- HoneywellScanner.offBarcodeReadSuccess = () => {
-    subscriptionBarcodeReadSuccess === null || subscriptionBarcodeReadSuccess === void 0 ? void 0 : subscriptionBarcodeReadSuccess.remove();
+HoneywellScanner.offBarcodeReadSuccess = () =>
+{
+    subscriptionBarcodeReadSuccess.remove();
 };
-HoneywellScanner.offBarcodeReadFail = () => {
-    subscriptionBarcodeReadFail === null || subscriptionBarcodeReadFail === void 0 ? void 0 : subscriptionBarcodeReadFail.remove();
+HoneywellScanner.offBarcodeReadFail = () =>
+{
+    subscriptionBarcodeReadFail.remove();
 };
 
 export default HoneywellScanner;
